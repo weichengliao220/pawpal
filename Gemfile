b2dba53f526fe721b8aa262d0eff0d9a97c53174
Roman HaDuong <roman.haduong@gmail.com>
2024-11-20 10:40:30 +0900

    seed done

--- Gemfile
+++ Gemfile
@@ -75,3 +75,4 @@ end
 
 gem "devise"
 gem "cloudinary"
+gem 'faker'
--- app/models/petsitter.rb
+++ app/models/petsitter.rb
@@ -1,8 +1,8 @@
 class Petsitter < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  has_many :bookings
+  belongs_to :user
+  has_many :bookings, dependent: :destroy
 
   validates :price, presence: true
-  validates :acceptable_pets, presence: true, inclusion: { in: %w(dog cat bird fish), message: "%{value} is not a valid pet type" }
+  validates :acceptable_pets, presence: true
   validates :bio, presence: true, length: { minimum: 5 }
 end
