2332a4bb2c704b0214eeb945544457694156bd28
Esteban Lopez <esteban.steam21@gmail.com>
2024-11-22 12:39:38 +0900

    sign_up_form_params_added

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :address, presence: true
+  has_one_attached :photo
+  validates :photo, presence: true
 end
